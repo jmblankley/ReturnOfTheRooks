212a0230df91e89413f9a75ac931ce56e4f153a8
jmblankley <117760865+jmblankley@users.noreply.github.com>
2023-02-13 17:00:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int newRooks;
 
 // iterate through the grid in the file
 // print out each element
+// PARAMETERS - instream, number of rows and cols to iterate through.
+// return void
 void readFile(ifstream &input, int rowCount, int colCount)
 {
     for (int row = 0; row < rowCount; row++)
@@ -28,6 +30,7 @@ void readFile(ifstream &input, int rowCount, int colCount)
 // take in an input file from user
 // store the first two ints in vars row and col
 // iterate through the grid in the file
+// PARAMETERS - number of rows and cols in the text file
 void printGrid(int rowCount, int colCount)
 {
     for (int row = 0; row < rowCount; row++)
@@ -40,19 +43,24 @@ void printGrid(int rowCount, int colCount)
     }
 }
 
-void clearBoard(int rowCount, int colCount)
+// looks for any 'R' in the 2d array and replaces it back with a '.' so we essentially get an empty board.
+void clearBoard()
 {
-    for(int rowClear = 0; rowClear < rowCount; rowClear++)
+    for (int rowClear = 0; rowClear < row; rowClear++)
     {
-        for(int colClear = 0; colClear < colCount; colClear++)
-        {   
-            rooks[rowClear][colClear] = ' ';
+        for (int colClear = 0; colClear < col; colClear++)
+        {
+            if (rooks[rowClear][colClear] == 'R')
+            {
+                rooks[rowClear][colClear] = '.';
+            }
         }
     }
 }
 
 // starting with trying at [0,0] check behind in the row and behind in the col
 // if there are no 'R' in the row or col return true
+// PARAMETERS - the row and col we are starting on
 bool canPlace(int currentRow, int currentCol)
 {
     // check if the spot you are looking is empty
@@ -67,6 +75,7 @@ bool canPlace(int currentRow, int currentCol)
     }
     if (rooks[currentRow][currentCol] == EMPTY)
     {
+        // look behind the current location in column
         for (int rowCheck = currentRow - 1; rowCheck >= 0; rowCheck--)
         {
             if (rooks[rowCheck][currentCol] == '#')
@@ -78,6 +87,7 @@ bool canPlace(int currentRow, int currentCol)
                 return false;
             }
         }
+        // look behind the current location in the row
         for (int colCheck = currentCol - 1; colCheck >= 0; colCheck--)
         {
             if (rooks[currentRow][colCheck] == '#')
@@ -96,6 +106,7 @@ bool canPlace(int currentRow, int currentCol)
 }
 
 // THINK VERY VERY VERY HARD ABOUT WHAT PARAMETERS!!!!!!!!!!!!!
+// PARAMETERS - number of rooks we are trying to place, the starting row and starting column
 bool solveBoard(int numRooks, int rowCur, int colCur)
 {
     // WE NEED A BASE CASE!
@@ -105,32 +116,27 @@ bool solveBoard(int numRooks, int rowCur, int colCur)
         return true;
     }
     // LOOP OVER ALL POSSIBLE NEXT MOVES!!!
-    while(numRooks >= 0)
-    { 
+    // look in every row in every column starting with current location
     for (int rowCheck = rowCur; rowCheck < row; rowCheck++)
     {
-        for (int colCheck = 0; colCheck < col; colCheck++)
+        for (int colCheck = colCur; colCheck < col; colCheck++)
         {
             if (canPlace(rowCheck, colCheck))
             {
                 rooks[rowCheck][colCheck] = 'R';
-                while (colCheck <= col)
+                if (solveBoard(numRooks - 1, rowCheck, colCheck + 1))
                 {
-                    if (solveBoard(numRooks - 1, rowCheck, colCheck + 1))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        rooks[rowCheck][colCheck] = rooks[rowCheck][colCheck];
-                    }
-                }                    
+                    return true;
+                }
+                else
+                {
+                    // puts back an empty space when it tries to put a rook and does not work out
+                    rooks[rowCheck][colCheck] = '.';
+                }
             }
         }
-        
+        colCur = 0;
     }
-    }
-    
 
     return false;
 }
@@ -138,12 +144,13 @@ bool solveBoard(int numRooks, int rowCur, int colCur)
 int main(int argc, char *argv[])
 {
 
+    // takes user input to open file
     string fname;
     cout << "Please enter the name of an input file: " << fname;
     cin >> fname;
-
     ifstream reader(fname);
 
+    // kicks out an error if you enter the name in worng, or if the file is empty
     if (!reader)
     {
 
@@ -157,31 +164,55 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    // takes our user input file and store the first two integers in the row and col variables
     reader >> row >> col;
 
-    
     readFile(reader, row, col);
-    printGrid(row, col);
 
-
+    // ask the user how many rooks they want to try and place
     cout << "How many rooks would you like to place? ";
     cin >> nRooks;
-    if (nRooks == -1)
-            {
-                cout << "Thank you for using RookPlacer - hope to see you again soon!" << endl;
-                return 0;
-            }
+
+    // see if you can solve the board with that many rooks
     if (solveBoard(nRooks, 0, 0))
     {
+        cout << "Yes, could place " << nRooks << " rooks." << endl;
         printGrid(row, col);
+        nRooks = 0;
     }
-    /*if(!solveBoard(nRooks, 0, 0))
+    clearBoard();
+    if (!solveBoard(nRooks, 0, 0) && nRooks != -1)
     {
-        cout << "Could not place " << nRooks << " rooks." << endl;
-    }*/
-   
+        cout << "No, could not place " << nRooks << " rooks." << endl;
+        nRooks = 0;
+    }
 
+    // after asking the user the first time, give them the option to enter in -1 and stop the program
+    // keep asking the user for a number of rooks until they enter -1 to stop the program
+    while (nRooks != -1)
+    {
+        cout << "How many rooks would you like to place (-1 to stop)? ";
+        cin >> nRooks;
 
+        if (solveBoard(nRooks, 0, 0))
+        {
+            cout << "Yes, could place " << nRooks << " rooks." << endl;
+            printGrid(row, col);
+            nRooks = 0;
+        }
+        clearBoard();
+        if (!solveBoard(nRooks, 0, 0) && nRooks != -1)
+        {
+            cout << "No, could not place " << nRooks << " rooks." << endl;
+            nRooks = 0;
+        }
+    }
+
+    // if the user enters the stopping number then stop the program and print out
+    if (nRooks == -1)
+    {
+        cout << "Thank you for using RookPlacer - hope to see you again soon!" << endl;
+    }
 
     return 0;
 }
